797b0e53fd14f863f647a6d825a1547c005d980e
Nicolas Fernandez <fernandez.nico91@gmail.com>
2021-02-07 01:51:55 -0300

    Hice mas tipos de visita, tengo que asegurarme q esten todos

--- SFR.py
+++ SFR.py
@@ -46,53 +46,53 @@ Numero_de_sitio=ws["M2"].value
 Nombre_de_archivo=str(Numero_de_sitio)+' COV Site File Review.xlsx'
 
 # Voy copiando las columnas que necesito
-# Excel={}
-# Encabezados=['Level','Category','Document Type','Ref Model Subtype','Study Item Name','Ref Model ID','Site Personnel Name','Document Date','Expiration Date']
-# Max_Column=ws.max_column
+Excel={}
+Encabezados=['Level','Category','Document Type','Ref Model Subtype','Study Item Name','Ref Model ID','Site Personnel Name','Document Date','Expiration Date']
+Max_Column=ws.max_column
 
-# # Busco usando la lista de encabezados, las coordenadas
-# for Encabezados_de_colunmnas in Encabezados:
-#     Excel[Encabezados_de_colunmnas]=[]
-#     for col_num in range (1,Max_Column):
-#         if ws.cell(row=1,column=col_num).value==Encabezados_de_colunmnas:            
-#             Letra_de_columna=get_column_letter(col_num)
-#             if ws.cell(row=1,column=col_num).value=='Document Date' or ws.cell(row=1,column=col_num).value=='Expiration Date':
-#                 for cell in ws[Letra_de_columna]:
-#                         try: #los que son "None"
-#                             cell.value=cell.value.strftime('%d-%b-%Y')
-#                         except:
-#                             continue
-#             for cell in ws[Letra_de_columna]:
-#                 Excel[Encabezados_de_colunmnas].append(cell.value)
-# # Con esto ya saque toda la info del REPORT.xlsx y lo tengo en el diccionario "Excel"
+# Busco usando la lista de encabezados, las coordenadas
+for Encabezados_de_colunmnas in Encabezados:
+    Excel[Encabezados_de_colunmnas]=[]
+    for col_num in range (1,Max_Column):
+        if ws.cell(row=1,column=col_num).value==Encabezados_de_colunmnas:            
+            Letra_de_columna=get_column_letter(col_num)
+            if ws.cell(row=1,column=col_num).value=='Document Date' or ws.cell(row=1,column=col_num).value=='Expiration Date':
+                for cell in ws[Letra_de_columna]:
+                        try: #los que son "None"
+                            cell.value=cell.value.strftime('%d-%b-%Y')
+                        except:
+                            continue
+            for cell in ws[Letra_de_columna]:
+                Excel[Encabezados_de_colunmnas].append(cell.value)
+# Con esto ya saque toda la info del REPORT.xlsx y lo tengo en el diccionario "Excel"
 
-# #Abro el archivo TEMPLATE.xlsx para pegar la info sacada de REPORT.xlsx.
+#Abro el archivo TEMPLATE.xlsx para pegar la info sacada de REPORT.xlsx.
 
-# wb = openpyxl.load_workbook('TEMPLATE.xlsx')    
-# ws=wb['Site']
-# Max_Row=ws.max_row
-# Max_Column=ws.max_column
+wb = openpyxl.load_workbook('TEMPLATE.xlsx')    
+ws=wb['Site']
+Max_Row=ws.max_row
+Max_Column=ws.max_column
 
-# # Antes de cualquier cosa, los encabezados del template y del report no son iguales, asi que hago la conversion asi no hay problema
+# Antes de cualquier cosa, los encabezados del template y del report no son iguales, asi que hago la conversion asi no hay problema
 
-# Excel['Class']=Excel['Level']
-# Excel['Document Category']=Excel['Category']
-# Excel['Document Name']=Excel['Study Item Name']
-# Excel['Site personnel name']=Excel['Site Personnel Name']
-# Excel['Document date']=Excel['Document Date']
-# Excel['Expiration date']=Excel['Expiration Date']
-# # Una vez esto hecho, a agregar el contenido de esas listas al fondo de cada columna
-# # Parseo el diccionario vs los encabezados
+Excel['Class']=Excel['Level']
+Excel['Document Category']=Excel['Category']
+Excel['Document Name']=Excel['Study Item Name']
+Excel['Site personnel name']=Excel['Site Personnel Name']
+Excel['Document date']=Excel['Document Date']
+Excel['Expiration date']=Excel['Expiration Date']
+# Una vez esto hecho, a agregar el contenido de esas listas al fondo de cada columna
+# Parseo el diccionario vs los encabezados
 
-# for key in Excel.keys():
-#     for column_num in range (1,Max_Column): #Reviso todos los encabezados
-#         if key==ws.cell(1,column_num).value: #Si el encabezado coincide con la key que tengo
-#             for row_num in range (1,len(Excel[key])): 
-#                 ws.cell(column=column_num,row=Max_Row+row_num-2).value=Excel[key][row_num] #Agrego todos los valores de la lista al final del ws
+for key in Excel.keys():
+    for column_num in range (1,Max_Column): #Reviso todos los encabezados
+        if key==ws.cell(1,column_num).value: #Si el encabezado coincide con la key que tengo
+            for row_num in range (1,len(Excel[key])): 
+                ws.cell(column=column_num,row=Max_Row+row_num-2).value=Excel[key][row_num] #Agrego todos los valores de la lista al final del ws
 
-#Guardo todo antes del procesador
+# Guardo todo antes del procesador
 
-# wb.save(Nombre_de_archivo)
+wb.save(Nombre_de_archivo)
 
 #TODO
 #Procesado
@@ -143,8 +143,9 @@ if 'VISIT REPORT.csv' in os.listdir('.') and 'VISIT REPORT.xlsx' not in os.listd
 class Sitio:
     def add_atribute(atribute, info_to_add):
         '''Esta funcion sirve para poder crear un atribute y asignarle un value llamado "info_to_add. Esto es especialmente util si estoy agregando cosas en un for loop'''
-        atribute=atribute.replace(' ','_') #depuro los posibles caracteres q puedan joder al ponerle el atributo al objeto. TODO usar regex
-        atribute=atribute.replace('/','_')
+        if type(atribute)==str:
+            atribute=atribute.replace(' ','_') #depuro los posibles caracteres q puedan joder al ponerle el atributo al objeto. TODO usar regex
+            atribute=atribute.replace('/','_')
         if hasattr(Sitio,atribute):
             New_info=getattr(Sitio,atribute)
             New_info.append(info_to_add) #Siexiste el atributo, lo appendeo. Ya que siempre es una lista
@@ -158,16 +159,17 @@ class Sitio:
 #Agarro del Visit report y agrego al Sitio cada una de las visitas, usando como nombre de atributo el tipo de visita, y el atributo es la fecha..
 #El atributo es una lista, y si hay mas de una visita del mismo tipo, lo appendea
 Visit_Report= pd.read_excel( os.getcwd()+"\\VISIT REPORT.xlsx",header=0)
-for index_Visit_Report,row_Visit_Report in Visit_Report['Visit Type'].iteritems():
-    if  type(Visit_Report['Visit End'][index_Visit_Report])==datetime.datetime:
-        Visit_Report.loc[index_Visit_Report,'Visit End']=Visit_Report['Visit End'][index_Visit_Report].date()
-    elif  type(Visit_Report['Visit End'][index_Visit_Report])==str:
-        Visit_Report.loc[index_Visit_Report,'Visit End']=Str_to_date(Visit_Report['Visit End'][index_Visit_Report])
-    Sitio.add_atribute(Visit_Report['Visit Type'][index_Visit_Report], Visit_Report['Visit End'][index_Visit_Report])
-
+for index_Visit_Report,row_Visit_Report in Visit_Report['Visit Type'].iteritems():    
+    try:
+        Visit_Report.loc[index_Visit_Report,'Visit End']=Visit_Report.loc[index_Visit_Report,'Visit End'].strftime('%d-%b-%Y')
+        Sitio.add_atribute(Visit_Report['Visit Type'][index_Visit_Report], Visit_Report['Visit End'][index_Visit_Report])
+    except:
+        pass
 #Ahora parseo por el DF del excel
 def add_to_excel(Row_num,Present_in_eTMF,Comments,Action_needed,*Action):
     '''Esta Funcion sirve para agregar los comentarios al Excel. '''
+    wb = openpyxl.load_workbook(Nombre_de_archivo)    
+    ws=wb['Site']
     Row_num+=2 #para la df el primer index es 0, pero el excel arranca en 2
     if Present_in_eTMF=='N':
         Row_num=ws.max_row+1 #Si no esta presente, mando el comentario al fondo
@@ -176,28 +178,41 @@ def add_to_excel(Row_num,Present_in_eTMF,Comments,Action_needed,*Action):
     ws.cell(Row_num,13).value=Action_needed
     if Action_needed=='Y':
         ws.cell(Row_num,14).value=Action[0]
+    wb.save(Nombre_de_archivo)
 
 #Esto es para 05.04.03
 def add_visit_from_report(Ref_ID, Generic_Variable_in_the_loop):
+    '''Esta funcion checkea todas las visitas del reporte de visitas y se fija si estan en el archivo de SFR. Si estan, escribe en comments 
+    diciendo que es la carta, si no esta agrega al final una linea con la info de que es lo que falta'''
     Letter_Types=['Confirmation Letter','Follow-up Letter', 'Monitoring Report']
     for index, row in SFR['Ref Model ID'].iteritems():
+        if SFR.loc[index,'Document date']==None:
+            continue
         if (SFR.loc[index,'Ref Model ID']==Ref_ID and
-            SFR.loc[index,'Document date']== Generic_Variable_in_the_loop and 
-            SFR.loc[index,'Ref Model Subtype'] not in Letter_Types):
-            add_to_excel(index,'Y',f"Duplicated {(SFR.loc[index,'Ref Model Subtype'])} from {Generic_Variable_in_the_loop} visit",'Y','Errase Duplicated')
-            continue #Si tengo un duplicado, no va a estar en letter types xq ya fue popeado. 
-        if (SFR.loc[index,'Ref Model ID']==Ref_ID and
-            SFR.loc[index,'Document date']== Generic_Variable_in_the_loop):
+            SFR.loc[index,'Document date']== Str_to_date(Generic_Variable_in_the_loop)):
+            if SFR.loc[index,'Ref Model Subtype'] not in Letter_Types:
+                add_to_excel(index,'Y',f"Duplicated {(SFR.loc[index,'Ref Model Subtype'])} from {Generic_Variable_in_the_loop} visit",'Y','Errase Duplicated')
+                continue #Si tengo un duplicado, no va a estar en letter types xq ya fue popeado. 
+            else:
                 Letter_Types.remove(SFR.loc[index,'Ref Model Subtype'])
                 SFR.loc[index,'Ref Model ID']=np.nan
                 add_to_excel(index,'Y',f"{(SFR.loc[index,'Ref Model Subtype'])} from {Generic_Variable_in_the_loop} visit",'N')
     if Letter_Types!=[]:
         add_to_excel(0,'N',f'{Letter_Types} missing from {Generic_Variable_in_the_loop} visit','Y','Collect from Site') #el primer argumento no importa en este caso, ya que se va a a setear igual al fondo
 
-        
-for Visit_Report in Sitio.Site_Visit_Interim:
-    add_visit_from_report('05.04.03', Visit_Report)
-#TODO, HACER PARA EL RESTO. TAL VEZ ENCONTRA RUNA MANERA DE TRANSFORMAR lo que hice en una funcion y  repetirlo para cada tipo de visita?
+def check_and_add(code, atribute):
+    '''Esta Funcion agarra un Ref ID y se fija si en el objeto Sitio tengo un tipo de visita que corresponda a ese ID. Si esta, ejecuta add_visit_from_report'''
+    if hasattr(Sitio,atribute):
+        for Visit_Report in getattr(Sitio,atribute):
+            add_visit_from_report(code, Visit_Report)
+
+#TODO Agregar todo los tipos de visita
+
+check_and_add('05.01.04','Site_Visit_Selection')
+check_and_add('05.03.01','Site_Visit_Initiation')            
+check_and_add('05.04.03','Site_Visit_Interim')      
+check_and_add('05.04.08','Telephone_Closeout' )
+
 
 #los temp/CALIBRATION logs lo puedo checkear desde los reporte de IP vs IP RETURNED
 #si es local o central tmb lo puedo sacar del log 
@@ -260,4 +275,3 @@ for Visit_Report in Sitio.Site_Visit_Interim:
 
 
 # #Grabo el nuevo excel con otro nombre
-# wb.save(Nombre_de_archivo)
